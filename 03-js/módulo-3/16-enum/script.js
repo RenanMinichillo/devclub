698a06e681aa34d96f1f9cfd106adb7814c71eb8
renanminichillo <renan.frans@gmail.com>
2025-02-25 10:21:48 -0300

    (chore): atualizado aulas dos exercícios do módulo 3

---
+++ 03-js/módulo-3/16-enum/script.js
@@ -0,0 +1,21 @@
+/*
+    ENUM
+
+    Criar um grupo com algumas informações que são repetidas no código para evitar erros.
+
+    const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+    }
+*/
+
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
+GAME_OPTIONS.ROCK
+GAME_OPTIONS.PAPER
+GAME_OPTIONS.SCISSORS
